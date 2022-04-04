3eb2ebd20e4bc4e150079ce8b73d63e466611983
Nicolas Tessore <n.tessore@ucl.ac.uk>
2022-04-04 09:10:28 +0100

    use submodule for functions

--- threej/numba.py
+++ threej/numba.py
@@ -5,10 +5,10 @@ from numba import types
 from numba.extending import overload
 from numba.core.errors import TypingError
 
-import threej
+from . import _threejj
 
 
-@overload(threej._threejj, jit_options=dict(nogil=True, fastmath=True))
+@overload(_threejj._threejj, jit_options=dict(nogil=True, fastmath=True))
 def _(l2, l3, m2, m3, out):
     for a in l2, l3, m2, m3:
         if not isinstance(a, types.Number):
@@ -18,15 +18,15 @@ def _(l2, l3, m2, m3, out):
             or not isinstance(out.dtype, types.Float):
         raise TypingError('out must be float array')
 
-    return threej._threejj
+    return _threejj._threejj
 
 
-@overload(threej._threejj_00, jit_options=dict(nogil=True, fastmath=True))
+@overload(_threejj._threejj_00, jit_options=dict(nogil=True, fastmath=True))
 def _(l2, l3, thrcof):
-    return threej._threejj_00
+    return _threejj._threejj_00
 
 
-@overload(threej.threejj)
+@overload(_threejj.threejj)
 def _(l2, l3, m2, m3, out=None):
     if isinstance(out, types.Optional):
         out = out.type
@@ -36,10 +36,10 @@ def _(l2, l3, m2, m3, out=None):
             l1min = max(abs(l2-l3), abs(m2+m3))
             l1max = l2+l3
             n = max(int(l1max-l1min+1.1), 0)
-            return threej._threejj(l2, l3, m2, m3, out=np.empty(n))
+            return _threejj._threejj(l2, l3, m2, m3, out=np.empty(n))
     else:
         def threejj(l2, l3, m2, m3, out=None):
-            return threej._threejj(l2, l3, m2, m3, out=out)
+            return _threejj._threejj(l2, l3, m2, m3, out=out)
 
     return threejj
 
